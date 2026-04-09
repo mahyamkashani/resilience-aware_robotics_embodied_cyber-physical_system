5901b12309f4c61181beabc195ba134a336f2f56
Gysella Imrell <gysella.michelle@gmail.com>
2026-04-09 15:42:03 +0200

    latest version of implementation

--- my_webot_project/controllers/epuck_controller/components/wheel.py
+++
@@ -1,9 +0,0 @@
-class Wheel:
-    def __init__(self, robot, name):
-        self.motor = robot.getDevice(name)
-        self.motor.setPosition(float('inf'))
-        self.motor.setVelocity(0.0)
-        
-
-    def set_speed(self, speed):
-        self.motor.setVelocity(speed)
