54f812514e0bb172ae5e09f2735b35b3c78f4d82
Mahya <mahya.mkashani@gmail.com>
2026-04-23 13:21:22 +0200

    :construction: obstacle avoidance added

--- my_webot_project/controllers/pr2_controller/manual_run.py
+++ my_webot_project/controllers/pr2_controller/manual_run.py
@@ -4,6 +4,6 @@ from logger import log_result
 theta_crit = 0.85
 run_id = 1
 
-output = run_simulation("configs/experiment1.json", use_ros=True)
+output = run_simulation("configs/experiment3.json", use_ros=True)
 
 log_result(theta_crit, run_id, output)
